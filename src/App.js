140a125a7839a310538a27ddcbf5abb588322734
Daniela Küster <daniela.kuester@gmx.net>
2018-08-15 22:28:49 +0200

    Initialize new map

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import mapboxgl from 'mapbox-gl'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        {/*<header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>*/}
-      </div>
-    );
-  }
+
+    componentDidMount() {
+        var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+        mapboxgl.accessToken = 'pk.eyJ1IjoiZGFuaWVsYS1rdWVzdGVyIiwiYSI6ImNqam9jdjF1YTAxczQzcG9na3JrZmh2bnkifQ.zMFUT-fI5tsviUOPZsJxog';
+        this.map = new mapboxgl.Map({
+            container: "map",
+            style: 'mapbox://styles/mapbox/streets-v10'
+        });
+    }
+
+
+    render() {
+        return (
+            <div className="App">
+                {/*<header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                To get started, edit <code>src/App.js</code> and save to reload.
+                </p>*/}
+                <section>
+                    <div id="map" className="map">
+
+                    </div>
+                </section>
+            </div>
+        );
+    }
 }
 
 export default App;
