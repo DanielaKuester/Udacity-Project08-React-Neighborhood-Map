2c61d2d68019c7615c01e3fe74d1553b6a012a73
Daniela Küster <daniela.kuester@gmx.net>
2018-08-15 23:20:36 +0200

    Add marker/popup to map

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js'
 
 class App extends Component {
 
     componentDidMount() {
-        var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
         mapboxgl.accessToken = 'pk.eyJ1IjoiZGFuaWVsYS1rdWVzdGVyIiwiYSI6ImNqam9jdjF1YTAxczQzcG9na3JrZmh2bnkifQ.zMFUT-fI5tsviUOPZsJxog';
         this.map = new mapboxgl.Map({
             container: "map",
-            style: 'mapbox://styles/mapbox/streets-v10'
+            style: 'mapbox://styles/mapbox/streets-v10',
+            zoom: 13,
+            center: [-0.11832, 51.5093]
         });
+
+        this.marker = new mapboxgl.Marker({color: this.props.color, className: 'my-marker'})
+            .setLngLat([-0.11832, 51.5093])
+            .addTo(this.map)
+
+        this.popup = new mapboxgl.Popup({ offset: 40 })
+            .setLngLat([-0.11832, 51.5093])
+            .setText('Obviously, Mapbox thinks that the centre of London is somewher in the Thames.')
+            .addTo(this.map)
     }
 
 
@@ -25,7 +36,7 @@ class App extends Component {
                 </p>*/}
                 <section>
                     <div id="map" className="map">
-
+                        {/* Initialize map here */}
                     </div>
                 </section>
             </div>
