c70540229e9fa14f28a565b3cf6c63bc241a5497
Daniela Küster <daniela.kuester@gmx.net>
2018-08-16 16:33:28 +0200

    Style: Make font size in popups bigger

--- src/Map.js
+++ src/Map.js
@@ -64,7 +64,7 @@ class Map extends Component {
             .map((location) => (
                 // create the popup
                 location.popup = new mapboxgl.Popup({ offset: 40 })
-                .setText('Hello! I am a friendly poppy puppy popcorn popup.'),
+                .setText(location.name),
 
                 // create the marker
                 location.marker = new mapboxgl.Marker({color: this.state.color, className: 'my-marker', alt: `Marker pointing to ${this.props.name}`})
